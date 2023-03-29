9a5e022871b84b52368b1ddd99fe6768d4229901
RAHULFROST7 <rahulfrost777@gmail.com>
2023-03-29 10:13:16 +0530

    modified:   resources/getAnswers.py 	modified:   resources/transcriber.py

--- resources/getAnswers.py
+++ resources/getAnswers.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from pymongo import MongoClient
 
 def answers(question):
@@ -14,21 +13,5 @@ def answers(question):
 
     for i in data:
         collected = list(i.values())[1:]
-=======
-from pymongo import MongoClient
-
-def answers(question):
-    client = MongoClient('mongodb+srv://webinterview:12345@cluster0.unj3vql.mongodb.net/main?retryWrites=true&w=majority')
-    # print('connection successful');
-
-    db = client['main']
-    col = db['questions']
-
-    myquery = {'question':question}
-
-    data = col.find(myquery)
-
-    for i in data:
-        collected = list(i.values())[1:]
->>>>>>> 06df0b248b9478cffc78e50b274d2b4cb69cf689
+        
     return collected
--- resources/transcriber.py
+++ resources/transcriber.py
@@ -1,16 +1,15 @@
-#!/usr/bin/env python3
-
-# import getopt
 import re
-# import sys
 import torch
 import whisper
 
-from googletrans import Translator
-# import youtube_dl
 
 
-AUDIOFILE = r"D:\Projects and codes\Audio-transcriber-main\audio.mp3"  # Save audio file as audio.mp3
+AUDIOFILE = r"D:\Projects and codes\Audio-transcriber-main\audio 2.mp3"  # Save audio file as audio.mp3
+
+
+def banner(text):
+    # """Display a message when the script is working in the background"""
+    print(f"# {text} #")
 
 
 def check_device():
@@ -24,36 +23,19 @@ def check_device():
 
 def get_result():
     # """Get speech recognition model."""
-    model_name = input("Select speech recognition model name (tiny, base, small, medium, large): ")
-    # banner("Transcribing text")
+    # model_name = input("Select speech recognition model name (tiny, base, small, medium, large): ")
+    model_name = "tiny"
+    banner("Transcribing text")
     model = whisper.load_model(model_name, device=check_device())
     result = model.transcribe(AUDIOFILE)
-    format_result('transcription.txt', result["text"])
+#     format_result('transcription.txt', result["text"])
 
 
-def format_result(file_name, text):
-    # """Put a newline character after each sentence and prompt user for translation."""
-    format_text = re.sub('\.', '.\n', text)
-    with open(file_name, 'a', encoding="utf-8") as file:
-        # banner("Writing transcription to text file")
-        file.write(format_text)
-        translate_result('transcription.txt', 'translation.txt')
+# def format_result(file_name, text):
+#     # """Put a newline character after each sentence and prompt user for translation."""
+#     format_text = re.sub('\.', '.\n', text)
+#     with open(file_name, 'a', encoding="utf-8") as file:
+#         banner("Writing transcription to text file")
+#         file.write(format_text)
 
-
-def translate_result(org_file, trans_file):
-   
-    translator = Translator()
-    with open(org_file, 'r', encoding="utf-8") as transcription:
-        contents = transcription.read()
-        # banner("Translating text")
-        translation = translator.translate(contents)
-    with open(trans_file, 'a', encoding="utf-8") as file:
-        # banner("Writing translation to text file")
-        file.write(translation.text)
-
-
-def main():
-    get_result() 
-    
-if __name__ == "__main__":
-    main()
+get_result()  # Get audio transcription and translation if needed
